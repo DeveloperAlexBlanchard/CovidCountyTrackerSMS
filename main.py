62acb0f031feb923772c9b677740a0be5fca0faf
Alex Blanchard <102440945+DeveloperAlexBlanchard@users.noreply.github.com>
2022-05-10 12:31:12 -0700

    Scrape data from the API for needed information

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ import requests
 
 response = requests.get('https://api.covidactnow.org/v2/county/06085.json?apiKey=c8275ae6f8074670ab819ba7937aa71a')
 county_data = response.json()
+
+# Gathering Data from API
+state = county_data['state']
+county = county_data['county']
+weekly_new_cases = county_data['metrics']['weeklyNewCasesPer100k']
+population = county_data['population']
+one_dose = county_data['actuals']['vaccinationsInitiated']
+one_dose_percentage = county_data['metrics']['vaccinationsInitiatedRatio']
+vaccinated = county_data['actuals']['vaccinationsCompleted']
+vaccinated_percentage = county_data['metrics']['vaccinationsCompletedRatio']
+additional_dose = county_data['actuals']['vaccinationsAdditionalDose']
+additional_dose_percentage = county_data['metrics']['vaccinationsAdditionalDoseRatio']
+unvaccinated = int(population) - int(vaccinated)
